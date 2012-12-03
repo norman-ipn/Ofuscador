7de092d1e85842d62ce66f159cbb0038c3c95241
Antonio Feregrino Bolaños <antonio.feregrino@hotmail.es>
2012-12-02 20:04:30 -0600

    Recursos
    Creo que en la carpeta de recursos podremos guardar archivos de texto
    que frecuente usaremos, tales como la definición de funciones basura y
    el "renombramiento" de tipos de datos. Esto con la finalidad de tenerlos
    a la mano de una manera más ordenada.

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ FILE * salida; // Apuntador al resultado final
 
 
 /**
- * 
+ * Programa para ofuscar código en C
  * @param argc
  * @param argv Por este medio se enviarán los archivos necesarios para la ejecución, la forma será [Ofuscador entrada.c salida.c]
  * @return 
@@ -19,19 +19,19 @@ int main( int argc, char **argv )
 	++argv, --argc; 
 
 	if ( argc > 1 ){
-		/* Lector de la entrada */
+		/* Lector de la entrada del archivo a ofuscar */
 		entrada = fopen( argv[0], "r" );
 		/* Abrimos(creamos) el archivo sobre el cual vamos a escribir nuestro codigo ofuscado */
 		salida = fopen(argv[1], "w");
                 
-                // TODO: Código del ofuscador
+		
                 
-                fclose(entrada);
+		fclose(entrada);
 		fclose(salida);
 	}else{
-            printf("Error en la ejecucion del programa (malos parametros de entrada)\n");
-            return 1;
-        }	
+		printf("Error en la ejecucion del programa (malos parametros de entrada)\n");
+		return 1;
+	}	
 
 	return 0;
 }
---
+++ recursos/renaming.c
@@ -0,0 +1,3 @@
+typedef int it;typedef float flt;typedef double dd;typedef char ch;
+typedef short int its;typedef unsigned short int iuts; typedef long int itl;
+typedef signed char chs; typedef long double dld;
